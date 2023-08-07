cb735cb7b0e781979116be6aa13cb1dc726a3426
Alexsandro Souza <alexsandro.souza@workday.com>
2023-08-07 15:02:41 +0100

    Updates

---
+++ demos/new-bot.js
@@ -0,0 +1,10 @@
+var CHATBOT_EMAIL = "apssouza22@gmail.com"
+var APP_KEY ='2414319'
+
+function loadChatScript(url) {
+    let script = document.createElement('script');
+    script.type = 'module';
+    script.src = url;
+    document.head.appendChild(script);
+}
+loadChatScript("https://apssouza22.github.io/chat-commander-ui/js/chatbot-plugin.js");
